4522df7c3debb7fb0db67a0aa1525072ff51f2e8
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-07-01 17:27:40 +0530

    Added snapping component

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,10 +1,13 @@
+import { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
+import SnappingScroller from '../components/SnappingScroller/SnappingScroller';
 import { animations } from '../components/animations';
+import './stylesheets/Work.css';
 
 export default function Work() {
 	return (
 		<motion.main variants={animations.container} initial='hidden' animate='visible' exit='exit'>
-			<div>Work</div>
+			<SnappingScroller />
 		</motion.main>
 	);
 }
