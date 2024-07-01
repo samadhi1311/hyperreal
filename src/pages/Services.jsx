923655743ed844664528965423bc82b29e280723
nipunigamage <nipunikanishka255@gmail.com>
2024-07-01 17:26:58 +0530

    Added gradient cursor effect

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,14 @@
 import { motion } from 'framer-motion';
 import { animations } from '../components/animations';
+import MovingGradient from '../components/MovingGradient/MovingGradient';
 
 export default function Services() {
 	return (
 		<motion.main variants={animations.container} initial='hidden' animate='visible' exit='exit'>
-			<div>Services</div>
+			<div>
+				Services
+				<MovingGradient size={320} tailLength={150} duration={100} />
+			</div>
 		</motion.main>
 	);
 }
