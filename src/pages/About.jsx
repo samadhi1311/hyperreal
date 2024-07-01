5db63bfa08f2b50dcad4170d534edd08832fbfc2
nipunigamage <nipunikanishka255@gmail.com>
2024-07-01 17:27:17 +0530

    Added Bi-drectional component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
 import { motion } from 'framer-motion';
 import { animations } from '../components/animations';
+import BidirectionalScroller from '../components/BidirectionalScroller/BidirectionalScroller';
 
 export default function About() {
 	return (
 		<motion.main variants={animations.container} initial='hidden' animate='visible' exit='exit'>
-			<div>About</div>
+			<BidirectionalScroller />
 		</motion.main>
 	);
 }
