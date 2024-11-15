7db672c70b882773987c0143c74b06a81d28e0fa
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-15 19:29:30 +0530

    Added a component for scrolling page to top

--- components/ui/scroll-widget.tsx
+++ components/ui/scroll-widget.tsx
@@ -1,12 +1,36 @@
 'use client';
 
 import { HiArrowSmallUp } from 'react-icons/hi2';
-import { useScroll, motion } from 'motion/react';
+import { AnimatePresence, motion } from 'motion/react';
+import { fade } from '@/lib/animations/animations';
+import { useEffect, useState } from 'react';
 
 export default function ScrollWidget() {
+	const [visibility, setVisibility] = useState(false);
+
+	useEffect(() => {
+		const handleVisibility = () => {
+			if (window.scrollY > 500) {
+				setVisibility(true);
+			} else {
+				setVisibility(false);
+			}
+		};
+
+		window.addEventListener('scroll', handleVisibility);
+
+		return () => window.removeEventListener('scroll', handleVisibility);
+	});
 	return (
-		<div className='fixed bottom-8 right-8 z-[999] flex items-center justify-center overflow-clip rounded-[100%] bg-white p-4'>
-			<HiArrowSmallUp className='h-8 w-8 text-black' />
-		</div>
+		<AnimatePresence>
+			{visibility && (
+				<motion.div
+					className='fixed bottom-8 right-8 z-[999] flex items-center justify-center overflow-clip rounded-[100%] border border-white/20 bg-black p-3'
+					{...fade}
+					onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+					<HiArrowSmallUp className='h-8 w-8 text-white' />
+				</motion.div>
+			)}
+		</AnimatePresence>
 	);
 }
