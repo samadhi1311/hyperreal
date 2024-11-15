a4037123cc6bfbca5be3402020e558f9c87d28b0
nipunigamage <nipunikanishka255@gmail.com>
2024-11-15 20:22:08 +0530

    Updated NextJS configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-	/* config options here */
-	basePath: '/hyperreal',
-	output: 'export',
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
