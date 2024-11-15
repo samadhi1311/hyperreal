75142d3a8aef0bd4f1184d941b1d55e655d93dab
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-15 19:36:51 +0530

    Updated TailwindCSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,9 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import * as tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ['class'],
+	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+
+	plugins: [tailwindcssAnimate],
 } satisfies Config;
