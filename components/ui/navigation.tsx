1a55725376af4d0c2508f168413a1ea63f273fc1
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-15 19:31:35 +0530

    Added navigation menu component

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { useEffect } from 'react';
 import { PiArrowRight } from 'react-icons/pi';
 
-export default function Navigation() {
+export default function Navigation({ closeMenu }: { closeMenu: () => void }) {
 	const [scope, animate] = useAnimate();
 
 	async function NavAnimations() {
@@ -40,11 +40,11 @@ export default function Navigation() {
 	const MenuItem = ({ title, url }: { title: string; url: string }) => {
 		return (
 			<motion.li initial='initial' whileHover='hover' animate='animate' className='h-16 w-fit rounded-full capitalize opacity-0'>
-				<Link href={url} className='flex h-16 flex-col gap-16 overflow-hidden'>
+				<Link href={url} className='flex h-16 flex-col gap-10 overflow-hidden md:gap-16' onClick={closeMenu}>
 					<motion.span variants={menuItemText} className='py-2'>
 						{title}
 					</motion.span>
-					<motion.span variants={menuItemText} aria-hidden className='bg-gradient-to-tr from-violet-500 to-orange-300 bg-clip-text py-2 text-transparent'>
+					<motion.span variants={menuItemText} aria-hidden className='bg-gradient-to-tr from-violet-500 to-orange-400 bg-clip-text py-2 text-transparent'>
 						{title}
 					</motion.span>
 				</Link>
@@ -55,14 +55,14 @@ export default function Navigation() {
 	return (
 		<motion.div
 			ref={scope}
-			className='navigation grains fixed inset-0 z-50 flex h-screen w-full items-center justify-center bg-black/60 font-light text-white backdrop-blur-lg backdrop-saturate-200'
+			className='navigation grains fixed inset-0 z-50 flex h-svh w-full items-center justify-center bg-black/60 font-light text-white backdrop-blur-xl backdrop-saturate-200 md:h-screen'
 			{...menuSlide}>
-			<motion.nav className='mx-auto grid w-full grid-cols-3 gap-32 p-16 text-5xl capitalize'>
+			<motion.nav className='mx-auto my-32 grid w-full grid-cols-none grid-rows-3 gap-16 p-16 text-base capitalize md:grid-cols-3 md:grid-rows-none md:gap-32 md:text-lg lg:text-5xl'>
 				<PiArrowRight className='absolute h-8 w-8 opacity-0 transition-all duration-500 group-hover/link:opacity-100' />
 				<div className='group/section'>
 					<p className='mb-1 text-sm opacity-50 transition-opacity duration-500 group-hover/section:opacity-80'>Our website</p>
-					<hr className='mb-16 opacity-20 transition-opacity duration-500 group-hover/section:opacity-60' />
-					<motion.ul className='flex flex-col gap-16'>
+					<hr className='mb-4 origin-top-left opacity-20 transition-opacity duration-500 group-hover/section:opacity-60 md:mb-16' />
+					<motion.ul className='flex flex-row gap-16 md:flex-col'>
 						<MenuItem title='Home' url='/' />
 						<MenuItem title='Work' url='/work' />
 						<MenuItem title='About' url='/about' />
@@ -71,8 +71,8 @@ export default function Navigation() {
 
 				<div className='group/section'>
 					<p className='mb-1 text-sm opacity-50 transition-opacity duration-500 group-hover/section:opacity-80'>Contact us</p>
-					<hr className='mb-16 opacity-20 transition-opacity duration-500 group-hover/section:opacity-60' />
-					<motion.ul className='flex h-fit flex-col gap-16'>
+					<hr className='mb-4 opacity-20 transition-opacity duration-500 group-hover/section:opacity-60 md:mb-16' />
+					<motion.ul className='flex flex-row gap-16 md:flex-col'>
 						<MenuItem title='whatsapp' url='/contact' />
 						<MenuItem title='email' url='/contact' />
 					</motion.ul>
@@ -80,8 +80,8 @@ export default function Navigation() {
 
 				<div className='group/section'>
 					<p className='mb-1 text-sm opacity-50 transition-opacity duration-500 group-hover/section:opacity-80'>Socials</p>
-					<hr className='mb-16 opacity-20 transition-opacity duration-500 group-hover/section:opacity-60' />
-					<motion.ul className='flex h-fit flex-col gap-16'>
+					<hr className='mb-4 opacity-20 transition-opacity duration-500 group-hover/section:opacity-60 md:mb-16' />
+					<motion.ul className='flex flex-row gap-16 md:flex-col'>
 						<MenuItem title='instagram' url='/' />
 						<MenuItem title='facebook' url='/' />
 					</motion.ul>
