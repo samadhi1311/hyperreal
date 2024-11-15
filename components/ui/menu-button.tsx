04844d56baf6cd0fed349f131d4f07af8bed8f67
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-15 19:31:53 +0530

    Added a component to toggle navigation

--- components/ui/menu-button.tsx
+++ components/ui/menu-button.tsx
@@ -3,24 +3,27 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import Navigation from './navigation';
-import { menuButton } from '@/lib/animations/animations';
+import { initialFade, menuButton } from '@/lib/animations/animations';
 
 export default function MenuButton() {
 	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<>
-			<button className='fixed right-8 top-8 z-[999] w-24 overflow-hidden rounded-full bg-white p-2 text-black' onClick={() => setIsOpen(!isOpen)}>
-				<label htmlFor='menu' className='relative flex w-32 items-center px-4 py-3'>
-					<motion.p variants={menuButton} animate={isOpen ? 'open' : 'closed'} className='absolute bg-white'>
+			<motion.button
+				className='fixed right-8 top-8 z-[999] w-24 cursor-pointer overflow-hidden rounded-full border border-white/20 bg-black p-2 text-white'
+				onClick={() => setIsOpen(!isOpen)}
+				{...initialFade}>
+				<label htmlFor='menu' className='relative flex w-32 cursor-pointer items-center bg-black px-4 py-3'>
+					<motion.p variants={menuButton} animate={isOpen ? 'open' : 'closed'} className='absolute bg-black text-white'>
 						CLOSE
 					</motion.p>
-					<motion.p variants={menuButton} animate={!isOpen ? 'open' : 'closed'} className='absolute bg-white'>
+					<motion.p variants={menuButton} animate={!isOpen ? 'open' : 'closed'} className='absolute bg-black'>
 						MENU
 					</motion.p>
 				</label>
-			</button>
+			</motion.button>
 
-			<AnimatePresence>{isOpen && <Navigation />}</AnimatePresence>
+			<AnimatePresence>{isOpen && <Navigation closeMenu={() => setIsOpen(false)} />}</AnimatePresence>
 		</>
 	);
 }
