aa609535a8c05185283fe5bb4fb9081999e6a8fa
nipunigamage <nipunikanishka255@gmail.com>
2024-11-16 20:46:00 +0530

    Updated hoc for page definition

--- components/ui/page-wrapper.tsx
+++ components/ui/page-wrapper.tsx
@@ -4,5 +4,9 @@ import { motion } from 'motion/react';
 import { pageTransition } from '@/lib/animations/animations';
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
-	return <motion.div {...pageTransition}>{children}</motion.div>;
+	return (
+		<motion.main {...pageTransition} className='mx-auto px-4 md:px-8 lg:px-16 xl:px-32'>
+			{children}
+		</motion.main>
+	);
 }
