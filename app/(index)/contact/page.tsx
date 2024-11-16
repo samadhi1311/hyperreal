9e90c7c4f69e3e5ce188ab744aff5c98daa2a659
nipunigamage <nipunikanishka255@gmail.com>
2024-11-16 20:50:00 +0530

    Added placeholder pages

--- app/(index)/contact/page.tsx
+++ app/(index)/contact/page.tsx
@@ -3,12 +3,12 @@
 import PageWrapper from '@/components/ui/page-wrapper';
 import Section from '@/components/ui/section';
 
-export default function Work() {
+export default function Contact() {
 	return (
 		<PageWrapper>
 			<Section>
 				<div className='flex h-screen items-center justify-center'>
-					<h1 className='text-3xl font-semibold'>Work WIP</h1>
+					<h1 className='text-3xl font-semibold'>Contact WIP</h1>
 				</div>
 			</Section>
 		</PageWrapper>
