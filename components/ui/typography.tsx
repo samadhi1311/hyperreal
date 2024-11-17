e59519710e1d1c49262d5c015b9b81886c5868a4
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-17 11:33:27 +0530

    Added heading and paragraph components

---
+++ components/ui/typography.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+
+function H1({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <h1 className={cn('text-5xl font-medium lg:text-7xl', className)}>{children}</h1>;
+}
+
+function H2({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <h2 className={cn('text-3xl font-medium lg:text-5xl', className)}>{children}</h2>;
+}
+
+function H3({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <h3 className={cn('text-xl font-medium lg:text-3xl', className)}>{children}</h3>;
+}
+
+function H4({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <h4 className={cn('text-lg font-medium lg:text-xl', className)}>{children}</h4>;
+}
+
+function P1({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <p className={cn('text-lg font-light', className)}>{children}</p>;
+}
+
+function P2({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <p className={cn('text-base font-light', className)}>{children}</p>;
+}
+
+function P3({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <p className={cn('text-sm font-light', className)}>{children}</p>;
+}
+
+function P4({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <p className={cn('text-xs font-light uppercase tracking-wide text-white/60', className)}>{children}</p>;
+}
+
+export { H1, H2, H3, H4, P1, P2, P3, P4 };
