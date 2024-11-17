ae7159a9544cbbd71bf83ff7b8aba49d77dbb69a
nipunigamage <nipunikanishka255@gmail.com>
2024-11-17 11:33:51 +0530

    Removed excessive vertical spacing

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -5,20 +5,17 @@ import PageWrapper from '@/components/ui/page-wrapper';
 import FAQ from '@/components/faq';
 import Hero from '@/components/hero';
 import Pricing from '@/components/pricing';
-import Section from '@/components/ui/section';
 
 export default function Home() {
 	return (
 		<PageWrapper>
-			<Section>
-				<Hero />
+			<Hero />
 
-				<Features />
+			<Features />
 
-				<Pricing />
+			<Pricing />
 
-				<FAQ />
-			</Section>
+			<FAQ />
 		</PageWrapper>
 	);
 }
