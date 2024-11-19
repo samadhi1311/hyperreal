12a0b82a72e2c4451283119dc2b2cd8170bdd8d1
nipunigamage <nipunikanishka255@gmail.com>
2024-11-19 19:20:08 +0530

    Seperated to modules for export

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -2,39 +2,73 @@
 
 import Section from '@/components/ui/section';
 import { LuCheck, LuX } from 'react-icons/lu';
+import { TbServerBolt } from 'react-icons/tb';
 import { Card } from './card';
 import { motion } from 'motion/react';
-import { H2, H3, P2, P4 } from '@/components/ui/typography';
+import { H2, H3, P1, P2, P4 } from '@/components/ui/typography';
+import Link from 'next/link';
 
-type TableRowProps = {
+export type TableRowProps = {
 	title: string;
 	core: React.ReactNode;
 	business: React.ReactNode;
-	enterprise: React.ReactNode;
 };
 
-export default function Pricing() {
-	function TableRow({ title, core, business, enterprise }: TableRowProps) {
-		return (
-			<motion.tr
-				className='border-b border-b-white/20 transition-colors duration-500 ease-in-out hover:bg-white/5'
-				initial={{ opacity: 0, y: 20 }}
-				whileInView={{
-					opacity: [0, 1],
-					y: [20, 0],
-					transition: {
-						duration: 1,
-						ease: [0.645, 0.045, 0.355, 1],
-					},
-				}}>
-				<td className='[&amp;:has([role=checkbox])]:pr-0 p-5 align-middle'>{title}</td>
-				<td className='[&amp;:has([role=checkbox])]:pr-0 p-5 align-middle'>{core}</td>
-				<td className='[&amp;:has([role=checkbox])]:pr-0 p-5 align-middle'>{business}</td>
-				<td className='[&amp;:has([role=checkbox])]:pr-0 p-5 align-middle'>{enterprise}</td>
-			</motion.tr>
-		);
-	}
+function TableRow({ title, core, business }: TableRowProps) {
+	return (
+		<motion.tr
+			className='border-b border-b-white/20 transition-colors duration-500 ease-in-out hover:bg-white/5'
+			initial={{ opacity: 0, y: 20 }}
+			whileInView={{
+				opacity: [0, 1],
+				y: [20, 0],
+				transition: {
+					duration: 1,
+					ease: [0.645, 0.045, 0.355, 1],
+				},
+			}}>
+			<td className='[&amp;:has([role=checkbox])]:pr-0 p-2 align-middle md:p-4'>{title}</td>
+			<td className='[&amp;:has([role=checkbox])]:pr-0 p-2 align-middle md:p-4'>{core}</td>
+			<td className='[&amp;:has([role=checkbox])]:pr-0 p-2 align-middle md:p-4'>{business}</td>
+		</motion.tr>
+	);
+}
 
+function PricingTable() {
+	return (
+		<table className='w-full caption-bottom border-0'>
+			<thead className='[&amp;_tr]:border-b'>
+				<tr className='border-b border-white/50'>
+					<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-left align-middle md:px-4'></th>
+					<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-left align-middle md:px-4'>Core</th>
+					<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-left align-middle md:px-4'>Business</th>
+				</tr>
+			</thead>
+			<tbody className='[&amp;_tr:last-child]:border-0 capitalize'>
+				<TableRow title='Type' core='Static' business='Dynamic' />
+				<TableRow title='Pages' core='Up to 8' business='Up to 20' />
+				<TableRow title='Responsive Design' core={<LuCheck className='size-5' />} business={<LuCheck className='size-5' />} />
+				<TableRow title='SEO Optimization' core={<LuCheck className='size-5' />} business={<LuCheck className='size-5' />} />
+				<TableRow title='Maintenance' core={<LuCheck className='size-5' />} business={<LuCheck className='size-5' />} />
+				<TableRow
+					title='Custom Features'
+					core={
+						<span className='flex items-center gap-1'>
+							<LuCheck className='size-5' />*
+						</span>
+					}
+					business={<LuCheck className='size-5' />}
+				/>
+				<TableRow title='Content Management System' core={<LuX className='size-5' />} business={<LuCheck className='size-5' />} />
+				<TableRow title='Contact Form' core={<LuX className='size-5' />} business={<LuCheck className='size-5' />} />
+				<TableRow title='Payment Integration' core={<LuX className='size-5' />} business={<LuCheck className='size-5' />} />
+			</tbody>
+			<caption className='mt-8 text-left text-sm italic text-white/60'>* Some advanced features may require to upgrade to Business package</caption>
+		</table>
+	);
+}
+
+function Pricing() {
 	return (
 		<Section>
 			<motion.div
@@ -64,12 +98,12 @@ export default function Pricing() {
 					}}>
 					<Card>
 						<div className='flex flex-col space-y-2 p-8'>
-							<H3 className='text-xl font-medium leading-none tracking-tight md:text-2xl'>Core</H3>
+							<H3 className='w-fit bg-gradient-to-br from-violet-500 to-orange-400 bg-clip-text text-transparent'>Core</H3>
 							<P2 className=''>Quick, reliable solutions to establish your online presence.</P2>
 						</div>
 						<div className='p-8 pt-0'>
 							<P2 className='mt-4'>Starting from</P2>
-							<span className='text-3xl font-semibold md:text-5xl'>LKR 32, 000</span>
+							<H2>LKR 32, 000</H2>
 						</div>
 					</Card>
 				</motion.div>
@@ -86,17 +120,20 @@ export default function Pricing() {
 					}}>
 					<Card>
 						<div className='flex flex-col space-y-2 p-8'>
-							<H3 className='text-xl font-medium leading-none tracking-tight md:text-2xl'>Business</H3>
+							<H3 className='w-fit bg-gradient-to-tl from-violet-500 to-orange-400 bg-clip-text text-transparent'>Business</H3>
 							<P2 className=''>Complete e-commerce solution to grow your business.</P2>
 						</div>
 						<div className='p-8 pt-0'>
 							<P2 className='mt-4'>Starting from</P2>
-							<span className='text-3xl font-semibold md:text-5xl'>LKR 56, 000</span>
+							<H2>LKR 56, 000</H2>
 						</div>
 					</Card>
 				</motion.div>
+			</div>
+			<div className='relative mt-16 w-full overflow-hidden'>
+				<PricingTable />
 				<motion.div
-					className='h-full w-full'
+					className='mt-16 h-full w-full'
 					initial={{ opacity: 0, scale: 0.95 }}
 					whileInView={{
 						opacity: [0, 1],
@@ -107,40 +144,21 @@ export default function Pricing() {
 						},
 					}}>
 					<Card>
-						<div className='flex flex-col space-y-2 p-8'>
-							<H3 className='text-xl font-medium leading-none tracking-tight md:text-2xl'>Enterprise</H3>
-							<P2 className=''>Advanced, tailor-made web solution for your company.</P2>
-						</div>
-						<div className='p-8 pt-0'>
-							<P2 className='mt-4'>Starting from</P2>
-							<span className='text-3xl font-semibold md:text-5xl'>LKR 122, 000</span>
+						<div className='p-8 md:p-12'>
+							<TbServerBolt className='size-16' />
+							<P1 className='mt-4 text-2xl'>
+								For details on hosting expenses at <span className='bg-gradient-to-tr from-violet-500 to-orange-300 bg-clip-text text-transparent'>hyperreal</span>, visit our{' '}
+								<Link href='#' className='underline'>
+									pricing
+								</Link>{' '}
+								page.
+							</P1>
 						</div>
 					</Card>
 				</motion.div>
 			</div>
-			<div className='relative mt-8 w-full overflow-hidden'>
-				<table className='mt-10 w-full min-w-[420px] caption-bottom border-0'>
-					<thead className='[&amp;_tr]:border-b'>
-						<tr className='border-b border-white/50'>
-							<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 px-4 text-left align-middle'></th>
-							<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 px-4 text-left align-middle'>Core</th>
-							<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 px-4 text-left align-middle'>Advanced</th>
-							<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 px-4 text-left align-middle'>Entreprise</th>
-						</tr>
-					</thead>
-					<tbody className='[&amp;_tr:last-child]:border-0 capitalize'>
-						<TableRow title='Type' core='Static' business='Dynamic' enterprise='Fully Customizable' />
-						<TableRow title='Pages' core='Up to 5' business='Up to 10' enterprise='Up to 50' />
-						<TableRow title='Responsive Design' core={<LuCheck className='size-5' />} business={<LuCheck className='size-5' />} enterprise={<LuCheck className='size-5' />} />
-						<TableRow title='SEO Optimization' core={<LuCheck className='size-5' />} business={<LuCheck className='size-5' />} enterprise={<LuCheck className='size-5' />} />
-						<TableRow title='Maintenance' core={<LuCheck className='size-5' />} business={<LuCheck className='size-5' />} enterprise={<LuCheck className='size-5' />} />
-						<TableRow title='Custom Features' core='Limited' business={<LuCheck className='size-5' />} enterprise={<LuCheck className='size-5' />} />
-						<TableRow title='Content Management System' core={<LuX className='size-5' />} business={<LuCheck className='size-5' />} enterprise={<LuCheck className='size-5' />} />
-						<TableRow title='Contact Form' core={<LuX className='size-5' />} business={<LuCheck className='size-5' />} enterprise={<LuCheck className='size-5' />} />
-						<TableRow title='Payment Integration' core={<LuX className='size-5' />} business={<LuCheck className='size-5' />} enterprise={<LuCheck className='size-5' />} />
-					</tbody>
-				</table>
-			</div>
 		</Section>
 	);
 }
+
+export { Pricing, PricingTable };
