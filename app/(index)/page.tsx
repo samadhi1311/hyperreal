bb5b1aa91596f5d124dbb7464174d680edc1c1fb
nipunigamage <nipunikanishka255@gmail.com>
2024-11-19 19:24:33 +0530

    Added minor layout changes

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -4,7 +4,7 @@ import Features from '@/components/features';
 import PageWrapper from '@/components/ui/page-wrapper';
 import FAQ from '@/components/faq';
 import Hero from '@/components/hero';
-import Pricing from '@/components/pricing';
+import { Pricing } from '@/components/pricing';
 import Footer from '@/components/footer';
 
 export default function Home() {
