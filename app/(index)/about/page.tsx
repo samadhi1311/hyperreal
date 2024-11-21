d234981818e928e4dc5d2cb2042832d240372d46
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-21 18:02:23 +0530

    Updated about section

--- app/(index)/about/page.tsx
+++ app/(index)/about/page.tsx
@@ -4,7 +4,10 @@ import PageWrapper from '@/components/ui/page-wrapper';
 import ParallaxLogo from '@/components/parallax-logo';
 import Section from '@/components/ui/section';
 import Footer from '@/components/footer';
-import { H1, H2, P1, P2 } from '@/components/ui/typography';
+import { H1, H2, H3, P1, P2 } from '@/components/ui/typography';
+import { Card } from '@/components/card';
+import { LuCode2 } from 'react-icons/lu';
+import { LuPaintbrush } from 'react-icons/lu';
 
 export default function About() {
 	return (
@@ -36,6 +39,39 @@ export default function About() {
 
 						<div className='h-[30vh] w-full overflow-hidden rounded-2xl bg-white lg:h-[80vh]'>
 							<ParallaxLogo />
+						</div>
+					</div>
+				</Section>
+				<Section>
+					<H2>Team</H2>
+					<div className='grid grid-cols-1 gap-8 py-16 md:grid-cols-2'>
+						<div className='overflow-hidden'>
+							<Card>
+								<div className='grid grid-cols-2 p-8'>
+									<div className='flex size-64 items-center justify-center'>
+										<img src={'/hyperreal.svg'} className='aspect-square object-contain' alt='logo' />
+									</div>
+									<div className='flex h-64 w-full flex-col items-start justify-end gap-2'>
+										<LuCode2 className='size-6' />
+										<H3 className='bg-gradient-to-br from-violet-600 to-orange-400 bg-clip-text text-transparent'>Samadhi Gunasinghe</H3>
+										<P2>Product Manager, Software Developer</P2>
+									</div>
+								</div>
+							</Card>
+						</div>
+						<div className='overflow-hidden'>
+							<Card>
+								<div className='grid grid-cols-2 p-8'>
+									<div className='flex size-64 items-center justify-center'>
+										<img src={'/hyperreal.svg'} className='aspect-square object-contain' alt='logo' />
+									</div>
+									<div className='flex h-64 w-full flex-col items-start justify-end gap-2'>
+										<LuPaintbrush className='size-6' />
+										<H3 className='bg-gradient-to-br from-violet-600 to-orange-400 bg-clip-text text-transparent'>Nipuni Gamage</H3>
+										<P2>Marketing Manager, UI/UX Designer</P2>
+									</div>
+								</div>
+							</Card>
 						</div>
 					</div>
 				</Section>
