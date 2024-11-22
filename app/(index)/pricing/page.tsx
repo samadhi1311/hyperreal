95748993d00d8f124d06df5ae9b25c223d510dab
nipunigamage <nipunikanishka255@gmail.com>
2024-11-22 10:48:26 +0530

    Added a limited-time-offer tag

--- app/(index)/pricing/page.tsx
+++ app/(index)/pricing/page.tsx
@@ -65,13 +65,18 @@ export default function PricingPage() {
 							},
 						}}>
 						<Card>
-							<div className='flex flex-col space-y-2 p-8'>
+							<div className='absolute right-0 top-0 overflow-hidden rounded-bl-2xl bg-white/5 p-4 backdrop-blur-[1px]'>
+								<P4 className='bg-gradient-to-r from-violet-500 to-orange-400 bg-clip-text font-medium tracking-widest text-transparent'>Limited-time Offer</P4>
+							</div>
+							<div className='flex flex-col space-y-2 overflow-hidden p-8'>
 								<H3 className='w-fit bg-gradient-to-br from-violet-500 to-orange-400 bg-clip-text text-transparent'>Core</H3>
 								<P2 className=''>Quick, reliable solutions to establish your online presence.</P2>
 							</div>
 							<div className='p-8 pt-0'>
-								<P2 className='mt-4'>Starting from</P2>
-								<H2>LKR 32, 000</H2>
+								<P2 className='mt-4'>
+									Starting from <span className='line-through decoration-white/60'>LKR 32, 000</span>
+								</P2>
+								<H2>LKR 24, 000</H2>
 							</div>
 						</Card>
 					</motion.div>
@@ -87,13 +92,18 @@ export default function PricingPage() {
 							},
 						}}>
 						<Card>
+							<div className='absolute right-0 top-0 overflow-hidden rounded-bl-2xl bg-white/5 p-4 backdrop-blur-[1px]'>
+								<P4 className='bg-gradient-to-r from-violet-500 to-orange-400 bg-clip-text font-medium tracking-widest text-transparent'>Limited-time Offer</P4>
+							</div>
 							<div className='flex flex-col space-y-2 p-8'>
 								<H3 className='w-fit bg-gradient-to-tl from-violet-500 to-orange-400 bg-clip-text text-transparent'>Business</H3>
 								<P2 className=''>Complete e-commerce solution to grow your business.</P2>
 							</div>
 							<div className='p-8 pt-0'>
-								<P2 className='mt-4'>Starting from</P2>
-								<H2>LKR 56, 000</H2>
+								<P2 className='mt-4'>
+									Starting from <span className='line-through decoration-white/60'>LKR 56, 000</span>
+								</P2>
+								<H2>LKR 47, 000</H2>
 							</div>
 						</Card>
 					</motion.div>
@@ -114,17 +124,67 @@ export default function PricingPage() {
 					<P4 className='mb-2'>also best on the market</P4>
 					<H2>Hosting</H2>
 				</motion.div>
+				<div className='my-16 flex flex-col gap-6 lg:flex-row lg:gap-4'>
+					<motion.div
+						className='h-full w-full'
+						initial={{ opacity: 0, scale: 0.95 }}
+						whileInView={{
+							opacity: [0, 1],
+							scale: [0.9, 1],
+							transition: {
+								duration: 0.5,
+								ease: [0.645, 0.045, 0.355, 1],
+							},
+						}}>
+						<Card>
+							<div className='flex flex-col space-y-2 overflow-hidden p-8'>
+								<H3 className='w-fit bg-gradient-to-br from-violet-500 to-orange-400 bg-clip-text text-transparent'>Core</H3>
+								<P2 className=''>Reliable hosting for your online presence.</P2>
+							</div>
+							<div className='p-8 pt-0'>
+								<P2 className='mt-4'>Starting from just</P2>
+								<H2>LKR 760/month</H2>
+							</div>
+						</Card>
+					</motion.div>
+					<motion.div
+						className='h-full w-full'
+						initial={{ opacity: 0, scale: 0.95 }}
+						whileInView={{
+							opacity: [0, 1],
+							scale: [0.9, 1],
+							transition: {
+								duration: 0.5,
+								ease: [0.645, 0.045, 0.355, 1],
+							},
+						}}>
+						<Card>
+							<div className='flex flex-col space-y-2 p-8'>
+								<H3 className='w-fit bg-gradient-to-tl from-violet-500 to-orange-400 bg-clip-text text-transparent'>Business</H3>
+								<P2 className=''>Complete e-commerce solution to grow your business.</P2>
+							</div>
+							<div className='p-8 pt-0'>
+								<P2 className='mt-4'>Starting from just</P2>
+								<H2>LKR 1, 900/month</H2>
+							</div>
+						</Card>
+					</motion.div>
+				</div>
 				<div className='relative mt-16 w-full overflow-hidden'>
 					<table className='w-full caption-bottom border-0'>
 						<thead className='[&amp;_tr]:border-b'>
 							<tr className='border-b border-white/50'>
 								<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-center align-middle md:px-4'></th>
 								<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-left align-middle md:px-4'>Service</th>
-								<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-center align-middle md:px-4'>Free tier</th>
-								<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-center align-middle md:px-4'>Usage beyond Free tier*</th>
+								<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-center align-middle md:px-4'>Included</th>
+								<th className='[&amp;:has([role=checkbox])]:pr-0 h-12 p-2 text-center align-middle md:px-4'>Extra usage*</th>
 							</tr>
 						</thead>
 						<tbody className='[&amp;_tr:last-child]:border-0 capitalize'>
+							<TableRow technology='Domain' service='' free='Yes' extra='' />
+							<tr>
+								<td className='h-8'></td>
+							</tr>
 							<TableRow technology='Database' service='Reads' free='750, 000' extra='100, 000' />
 							<TableRow technology='' service='Row Writes' free='300, 000' extra='50, 000' />
 							<TableRow technology='' service='Row Deletes' free='300, 000' extra='50, 000' />
@@ -150,7 +210,7 @@ export default function PricingPage() {
 							</tr>
 							<TableRow technology='Web Traffic' service='Network Bandwidth' free='100 GB' extra='10 GB' />
 						</tbody>
-						<caption className='mt-8 text-left text-sm italic text-white/60'>* Usage beyond free tier is claculated per unit which costs LKR 100.</caption>
+						<caption className='mt-8 text-left text-sm italic text-white/60'>* Usage beyond the included is claculated per unit which costs LKR 100.</caption>
 					</table>
 					<motion.div className='mt-16 h-full w-full'>
 						<Card>
