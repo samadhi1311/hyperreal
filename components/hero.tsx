380c4007025a08b2737df8dc4dc840062451773b
nipunigamage <nipunikanishka255@gmail.com>
2024-11-22 16:58:40 +0530

    Added minor layout changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -107,7 +107,7 @@ export default function Hero() {
 
 	return (
 		<section className='flex h-svh w-full items-center justify-center'>
-			<div ref={scope} className='flex max-w-min flex-col items-center justify-center gap-16'>
+			<div ref={scope} className='flex flex-col items-center justify-center gap-16 md:max-w-min'>
 				<motion.span className='logo-container flex items-center gap-8 overflow-clip opacity-0'>
 					<motion.img className='hyperreal-logo h-8 w-8 max-w-none scale-[1.06] md:h-16 md:w-16' src='/hyperreal.svg' loading='lazy' width={32} height={32} alt='hyperreal logo' />
 					<motion.h1 className='hyperreal-heading funnel-display flex items-center text-5xl font-semibold leading-none text-white md:text-8xl'>
@@ -120,7 +120,7 @@ export default function Hero() {
 				</motion.span>
 
 				<motion.span className='flex items-center justify-center self-end overflow-clip text-center md:self-start'>
-					<motion.p className='slogan relative flex max-w-[300px] flex-wrap gap-1.5 text-lg font-light leading-none text-white md:max-w-[600px] md:gap-2 md:text-3xl'>
+					<motion.p className='slogan relative flex max-w-[300px] flex-wrap items-center justify-center gap-1.5 text-lg font-light leading-none text-white md:max-w-[600px] md:gap-2 md:text-3xl'>
 						{Array.from('we craft creative and efficient web solutions anyone can love'.split(' ')).map((word, index) => (
 							<motion.span
 								className={`slogan-words opacity-0 ${
@@ -133,7 +133,7 @@ export default function Hero() {
 					</motion.p>
 				</motion.span>
 
-				<motion.span className='cta-buttons flex items-center gap-4 self-start opacity-0'>
+				<motion.span className='cta-buttons flex items-center gap-4 self-center opacity-0'>
 					<Link href='/contact'>
 						<PrimaryButton>
 							<BiUserVoice className='mr-2 h-5 w-5 text-black' />
