051e517d24a6a0a7452ccdf721d6741bbbb1ee8c
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-28 21:48:38 +0530

    Updated styling for more contrast

--- components/menu-button.tsx
+++ components/menu-button.tsx
@@ -10,14 +10,14 @@ export default function MenuButton() {
 	return (
 		<>
 			<motion.button
-				className='fixed right-8 top-8 z-[999] w-24 cursor-pointer overflow-hidden rounded-full border border-white/10 bg-black p-2 text-white'
+				className='fixed right-8 top-8 z-[999] w-24 cursor-pointer overflow-hidden rounded-full border border-white/20 bg-white/5 p-2 text-white'
 				onClick={() => setIsOpen(!isOpen)}
 				{...initialFade}>
-				<label htmlFor='menu' className='relative flex w-32 cursor-pointer items-center bg-black px-4 py-3'>
-					<motion.p variants={menuButton} animate={isOpen ? 'open' : 'closed'} className='absolute bg-black text-white'>
+				<label htmlFor='menu' className='relative flex w-32 cursor-pointer items-center px-4 py-3'>
+					<motion.p variants={menuButton} animate={isOpen ? 'open' : 'closed'} className='absolute text-white'>
 						CLOSE
 					</motion.p>
-					<motion.p variants={menuButton} animate={!isOpen ? 'open' : 'closed'} className='absolute bg-black'>
+					<motion.p variants={menuButton} animate={!isOpen ? 'open' : 'closed'} className='absolute'>
 						MENU
 					</motion.p>
 				</label>
