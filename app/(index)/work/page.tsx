3996f3a706537d02fd227cda553f8b165190e50d
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-28 21:50:36 +0530

    Updated layout for content

--- app/(index)/work/page.tsx
+++ app/(index)/work/page.tsx
@@ -1,36 +1,31 @@
 'use client';
 
-import { H1, H2, P2, P3 } from '@/components/ui/typography';
+import { P1, P3 } from '@/components/ui/typography';
 import PageWrapper from '@/components/ui/page-wrapper';
 import Section from '@/components/ui/section';
 import Footer from '@/components/footer';
 import Link from 'next/link';
+import { LuArrowUpRight } from 'react-icons/lu';
 
 export default function Work() {
 	return (
 		<>
 			<PageWrapper>
 				<Section>
-					<H1>Our Work</H1>
-					<div className='grid grid-cols-1 gap-8 py-16 md:grid-cols-2'>
-						<div className='aspect-video h-full overflow-hidden rounded-2xl bg-white/5'>
-							<img src='/colorspeck.png' className='h-full w-full object-contain' alt='Colorspeck thumbnail' />
-						</div>
-						<div className='flex w-full flex-col justify-between'>
-							<div className='flex w-fit flex-col items-end gap-1'>
-								<H2 className='flex items-center gap-2'>
-									<img src='/colorspeck-logo.png' alt='Colorspeck logo' className='h-12 w-full' />
-								</H2>
-								<P3>
-									Visit{' '}
-									<Link href='https://colorspeck.pages.dev' className='underline'>
-										colorspeck.lk
-									</Link>
-								</P3>
-							</div>
-							<P2 className='max-w-prose'>An E-commerce platform for COLORSPECK to showcase and sell handicrafts worldwide. Build and maintained by hyperreal.</P2>
-						</div>
+					<div className='flex items-center justify-end'>
+						<P3 className=''>
+							<Link href='https://colorspeck.lk' className='flex items-center gap-2 duration-500 hover:text-white/80'>
+								visit <span className='underline'>colorspeck.lk</span>
+								<LuArrowUpRight className='size-4' />
+							</Link>
+						</P3>
 					</div>
+					<div data-orientation='horizontal' role='none' className='bg-border mb-8 mt-3 h-[1px] w-full shrink-0'></div>
+					<div className='flex flex-col justify-between gap-6 md:flex-row'>
+						<img src='/colorspeck-logo.png' alt='Colorspeck logo' className='h-16 w-auto' />
+						<P1 className='md:w-1/2'>An E-commerce platform for COLORSPECK to showcase and sell handicrafts worldwide. Build and maintained by hyperreal.</P1>
+					</div>
+					<video src='/colorspeck.mp4' about='Colorspeck' autoPlay loop controls={false} className='mt-16 aspect-video w-full rounded-lg object-cover' />
 				</Section>
 			</PageWrapper>
 			<Footer />
