42924719a2d9c98c45041075117b5835da5e54c6
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-12 12:51:18 +0530

    Added sitemap

---
+++ sitemap.ts
@@ -0,0 +1,42 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: 'https://hyperreal.cloud',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://hyperreal.cloud/about',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://hyperreal.cloud/contact',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://hyperreal.cloud/pricing',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://hyperreal.cloud/terms-and-conditions',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://hyperreal.cloud/work',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+	];
+}
