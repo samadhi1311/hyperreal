a003cb33ef8120ef737f80cf607320caeb02060d
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-01-05 18:49:11 +0530

    Updated padding

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -3,5 +3,5 @@
 import { cn } from '@/lib/utils';
 
 export default function Section({ children, className }: { children: React.ReactNode; className?: string }) {
-	return <section className={cn('w-full py-24 px-8 lg:px-0', className)}>{children}</section>;
+	return <section className={cn('w-full py-16 lg:py-24 px-8', className)}>{children}</section>;
 }
