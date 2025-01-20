4deb6b805b84361a929e16e72f8dd13078e7cc97
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-01-20 16:14:23 +0530

    Fixed z index

--- components/scroll-progress.tsx
+++ components/scroll-progress.tsx
@@ -9,5 +9,5 @@ export default function ScrollProgress() {
 		damping: 30,
 		restDelta: 0.001,
 	});
-	return <motion.div style={{ scaleX }} className='fixed left-0 right-0 top-0 z-10 h-1 origin-left bg-gradient-to-r from-violet-500 to-orange-300' />;
+	return <motion.div style={{ scaleX }} className='fixed left-0 right-0 top-0 z-[999] h-1 origin-left bg-gradient-to-r from-violet-500 to-orange-300' />;
 }
